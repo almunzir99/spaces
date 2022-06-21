d0d32a03df41707d373ff1b6f732a99c4d3a394a
almunzir99 <almunzir99@gmail.com>
2022-06-21 22:47:08 +0200

    design footer

--- Client/spaces/src/app/shared/components/footer/footer.component.ts
+++ Client/spaces/src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { navBarList } from '../nav-bar/navbar-list';
 
 @Component({
   selector: 'spaces-footer',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  links = navBarList;
   constructor() { }
 
   ngOnInit(): void {
