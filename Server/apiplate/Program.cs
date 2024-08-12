415452d45c918dd734f048cb196f50f5a5eb0dcf
almunzir99 <47689796+almunzir99@users.noreply.github.com>
2024-08-12 19:12:31 +0300

    Update Program.cs

--- Server/apiplate/Program.cs
+++ Server/apiplate/Program.cs
@@ -1,8 +1,10 @@
+using System.IO;
 using System.Threading.Tasks;
 using apiplate.StartupTasks.Extensions;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using System;
+
 namespace apiplate
 {
     public class Program
@@ -16,12 +18,17 @@ namespace apiplate
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                      var webRootPath = Environment.GetEnvironmentVariable("ASPNETCORE_WEBROOT");
-                
+                       var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                // Get the WebRootPath value from appsettings.json
+                var webRootPath = config.GetValue<string>("WebRootPath");
+
                 // Set the custom web root path
                 webBuilder.UseWebRoot(webRootPath);
                     webBuilder.UseStartup<Startup>();
-                    
                 });
     }
 }
